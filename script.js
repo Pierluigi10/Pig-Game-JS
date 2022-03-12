138a3a83ceee53b9029c4f4d676a0651c8b0a183
pierluigi <pierluigibaiano@gmail.com>
2022-03-12 12:42:17 +0100

    add functionality rolled 1

--- script.js
+++ script.js
@@ -7,6 +7,9 @@
 
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -17,6 +20,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+let currentScore = 0;
 
 // Rolling dice functionality
 
@@ -32,7 +36,9 @@ btnRoll.addEventListener("click", function () {
   // 3. Check for rolled 1
   if (dice !== 1) {
     // Add dice to current score
-
+    // currentScore = currentScore + dice;
+    currentScore += dice;
+    current0El.textContent = currentScore;
   } else {
     // Switch to next player
   }
