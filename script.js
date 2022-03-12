4c9a4429cdab19046bc8128fdf96ebdcb5aec2e7
pierluigi <pierluigibaiano@gmail.com>
2022-03-12 12:36:02 +0100

    start with dhe rolling functionality

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector("btn--new");
-const btnRoll = document.querySelector("btn--roll");
-const btnHold = document.querySelector("btn--hold");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 //Starting conditions
 score0El.textContent = 0;
@@ -18,3 +18,22 @@ score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
 
+// Rolling dice functionality
+
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice rolling
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `images/dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+
+  } else {
+    // Switch to next player
+  }
+});
