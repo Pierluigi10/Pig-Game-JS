7b3008c57a470070c9fc0bc2bea2576280a3207f
pierluigi <pierluigibaiano@gmail.com>
2022-03-12 12:46:12 +0100

    add activePlayer dinamicaly

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 
@@ -38,7 +40,8 @@ btnRoll.addEventListener("click", function () {
     // Add dice to current score
     // currentScore = currentScore + dice;
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
   }
